366500590d1254f81145097a7bf830a797086071
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-10-08 16:26:16 -0700

    header, footer, main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from './components/header/header.js';
 import Footer from './components/footer/footer.js';
+import Votetracker from './components/vote_tracker/vote_tracker.js';
+
 import './App.css';
 
 class App extends Component {
@@ -8,7 +10,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <Header></Header>
-
+        <Votetracker></Votetracker>
         <Footer></Footer>
       </React.Fragment>
     );
---
+++ src/components/vote_tracker/vote_tracker.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './vote_tracker.css';
+
+class Votetracker extends React.Component {
+    render() {
+        return (
+            <main>
+                <h1>Here is Proof of Life - Vote Tracker</h1>
+            </main>
+            )
+    }
+}
+
+export default Votetracker; 
